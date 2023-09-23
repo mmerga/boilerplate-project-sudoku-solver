4645e9ef429180b6029db6549a1342ffbf42e9ea
mmerga <78653901+mmerga@users.noreply.github.com>
2023-09-23 01:19:53 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,32 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+//////--------------x-x-x-x-x-x-x-x-x-x-------------\\\\\
+
+module.exports = function(app) {
+
+  let Sudoku = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
-
+      const puzzle = req.body.puzzle;
+      const coordinate = req.body.coordinate;
+      const value = req.body.value;
+      if(puzzle && coordinate && value ){
+        res.json(Sudoku.validate(puzzle, coordinate, value));
+      }else{
+        res.json({ error: 'Required field(s) missing' });
+      }
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-
+      const puzzle = req.body.puzzle;
+      if(puzzle){
+        const aux = Sudoku.solve(puzzle);
+        res.json(aux);
+      }else{
+         res.json({ error: 'Required field missing' });
+      }
     });
 };
